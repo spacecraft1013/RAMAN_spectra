b303dcbf5372aa84481ce6cfb166fe8e1cd45929
Amin Boukari <ambouk3@gmail.com>
2022-04-03 14:49:50 -0400

    Validation accuracy graphs

---
+++ graphs.py
@@ -0,0 +1,23 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib
+
+results_df = pd.read_csv('results_noweights_reference.csv')
+
+sns.set_theme(context='notebook', style='darkgrid', palette='deep', font_scale=1.3)
+
+plot = sns.catplot(
+    data=results_df,
+    kind='bar',
+    x='Average Size',
+    y='Validation Accuracy',
+    hue='Mode',
+    legend=True,
+    legend_out=True
+)
+plot.fig.subplots_adjust(top=.95)
+plot.ax.set_title('Validation Accuracy by Average Size and Averaging Method')
+
+plt.savefig('./figures/accuracy_plots.png', dpi=1000, transparent=True)
+plt.show()
